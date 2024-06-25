6fcdba83d7d5a4f1c49de92fa057eea8edb54ee7
Juan Nieto <jnietos@live.com>
2024-06-25 10:23:54 -0500

    Arreglado diseño responsivo para página de login

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,11 +1,11 @@
-<div class="min-h-screen flex flex-row sm:justify-center items-center pt-6 sm:pt-0 bg-light-green">
+<div class="min-h-screen flex flex-col md:flex-row sm:justify-center items-center pt-6 sm:pt-0 bg-light-green">
 
-    <div class="flex items-stretch -mx-4">
-        <div class="w-full sm:max-w-md mt-6 mx-5 shadow-md overflow-hidden sm:rounded-lg">
+    <div class="md:flex items-stretch">
+        <div class="md:w-full sm:max-w-sm mt-6 mx-5 shadow-md overflow-hidden rounded-lg">
             <img class="object-cover h-full align-middle" src="{{ URL::to('/assets/img/Hospital.jpeg') }}">
         </div>
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="md:w-full sm:max-w-sm mt-6 mx-5 md:mx-0 px-6 py-4 bg-white shadow-md overflow-hidden rounded-lg">
             {{ $slot }}
         </div>
     </div>
--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-sm text-white tracking-widest hover:bg-gray-700 focus:bg-green-700 active:bg-green-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-sm text-white tracking-widest hover:bg-green-700 focus:bg-green-700 active:bg-green-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 disabled:opacity-50 transition ease-in-out duration-150']) }}>
     {{ $slot }}
 </button>
