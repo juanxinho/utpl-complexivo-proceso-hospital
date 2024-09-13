0fcdf76dd0f7ba4516b4b21233751acc3e4f58ef
Juan Nieto <jnietos@live.com>
2024-09-13 09:27:38 -0500

    Oculto bloque en home de Medic de Records

--- resources/views/welcome/medic.blade.php
+++ resources/views/welcome/medic.blade.php
@@ -19,7 +19,7 @@
         </div>
 
         <!-- Patient Records Card -->
-        <div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        {{--<div class="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <div class="p-5">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ __('Patient Records') }}</h5>
                 <p class="mb-3 font-normal text-gray-700">{{ __('Check Patient Records') }}</p>
@@ -27,7 +27,7 @@
                     {{ __('View Patient Records') }}
                 </x-button-link>
             </div>
-        </div>
+        </div>--}}
 
     </div>
 </x-app-layout>
